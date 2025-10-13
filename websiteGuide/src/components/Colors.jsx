e74c07576619fae717fc1452df3622db3b77039d
Stephanie Mei <smei826@gmail.com>
2025-10-13 16:10:24 -0400

    color section fixed

--- websiteGuide/src/components/Colors.jsx
+++ websiteGuide/src/components/Colors.jsx
@@ -3,9 +3,12 @@ import "./Colors.css";
 export default function Colors() {
   return (
     <div className="color-container">
-      <div className="color one"></div>
-      <div className="color two"></div>
-      <div className="color three"></div>
+      <h3>COLORS</h3>
+      <div className="colors">
+        <div className="color one">Primary Color</div>
+        <div className="color two">Secondary Color</div>
+        <div className="color three">Accent Color</div>
+      </div>
     </div>
   );
 }
--- websiteGuide/src/components/Fonts.jsx
+++ websiteGuide/src/components/Fonts.jsx
@@ -1,6 +1,8 @@
+import "./Fonts.css";
+
 export default function Fonts() {
   return (
-    <div>
+    <div className="fonts-container">
       <div>Font 1</div>
       <div>Font 2</div>
     </div>
