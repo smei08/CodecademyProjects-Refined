7552fd8be8766c27de5cbe124a2837596b3ee786
Stephanie Mei <smei826@gmail.com>
2025-10-17 14:04:36 -0400

    font added

--- websiteGuide/src/components/Fonts.jsx
+++ websiteGuide/src/components/Fonts.jsx
@@ -3,8 +3,39 @@ import "./Fonts.css";
 export default function Fonts() {
   return (
     <div className="fonts-container">
-      <div>Font 1</div>
-      <div>Font 2</div>
+      <h3>FONTS</h3>
+      <div className="fonts">
+        <div className="font f1">
+          <p className="title">Font 1</p>
+          <p>The quick brown fox jumps over the lazy dog.</p>
+          <p style={{ fontWeight: "bold" }}>
+            The quick brown fox jumps over the lazy dog.
+          </p>
+          <p style={{ fontStyle: "italic" }}>
+            The quick brown fox jumps over the lazy dog.
+          </p>
+        </div>
+        <div className="font f2">
+          <p className="title">Font 2</p>
+          <p>The quick brown fox jumps over the lazy dog.</p>
+          <p style={{ fontWeight: "bold" }}>
+            The quick brown fox jumps over the lazy dog.
+          </p>
+          <p style={{ fontStyle: "italic" }}>
+            The quick brown fox jumps over the lazy dog.
+          </p>
+        </div>
+        <div className="font f3">
+          <p className="title">Font 3</p>
+          <p>The quick brown fox jumps over the lazy dog.</p>
+          <p style={{ fontWeight: "bold" }}>
+            The quick brown fox jumps over the lazy dog.
+          </p>
+          <p style={{ fontStyle: "italic" }}>
+            The quick brown fox jumps over the lazy dog.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
